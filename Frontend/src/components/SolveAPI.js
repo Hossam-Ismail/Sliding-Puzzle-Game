8212c062884cef9574f50d3beeed821d392ce8e8
MohamedAIsmail <mohamedaismail214@gmail.com>
2024-09-08 11:56:32 +0300

    chore: changing the api url to be taken from the env variable

--- Frontend/src/components/SolveAPI.js
+++ Frontend/src/components/SolveAPI.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const PUZZLE_SOLVE_API_URL = process.env.REACT_APP_PUZZLE_SOLVER_API;
+
 export const handleSolvePuzzle = async (puzzle) => {
   try {
-    const response = await axios.post("http://localhost:5000/solve-puzzle", {
+    const response = await axios.post(PUZZLE_SOLVE_API_URL, {
       puzzle,
     });
     return response.data;
